a2f52ae695c97560129b304afec3214912214f9d
Rix565 <61545240+Rix565@users.noreply.github.com>
2021-07-16 19:27:34 +0200

    Whoops, forgot to delete debugging stuff

--- ucinclude/db/con.php
+++ ucinclude/db/con.php
@@ -1,10 +1,10 @@
 <?php
 session_start(); 
-//try {
+try {
     $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME , USER, PASS);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//} catch (PDOException $e) {
-    //echo 'A database error occured when loading UnderCMS.' ;
-    //exit;
-//}
+} catch (PDOException $e) {
+    echo 'A database error occured when loading UnderCMS.' ;
+   /exit;
+}
 ?>
