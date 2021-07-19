b2e3f50526091767c4a4db828381d49a5ccbc18d
Rix565 <61545240+Rix565@users.noreply.github.com>
2021-07-19 19:24:56 +0200

    Delete now useless dir

--- ucinclude/user/createuser.php
+++
@@ -1,8 +0,0 @@
-<?php
-//This file is only used to create an usrer
-function createUser($username, $password, $email, $db){
-    $hashpass = password_hash($password, PASSWORD_ARGON2ID);
-    $query = $db->prepare("INSERT INTO `" . DB_PREFIX . "users`(`username`, `password`, `email`, `datecreated`) VALUES  ('".$username."','". $hashpass. "','".$email."','". time() ."')");
-    $query->execute();
-}
-?>
