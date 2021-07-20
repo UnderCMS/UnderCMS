8842adea7128d278fbbf542188eaf2121a74c1cf
Rix565 <61545240+Rix565@users.noreply.github.com>
2021-07-20 18:46:05 +0200

    Whoops forgot this

--- ucadmin/index.php
+++ ucadmin/index.php
@@ -6,7 +6,7 @@ $user = $userclass->getUserDataByToken($_SESSION['token'], $db);
     <section class="row mb-5 pb-md-4 align-items-center">
         <div class="col-md-5">
             <div class="masthead-followup-icon d-inline-block mb-2">        
-                <h2 class="display-5 fw-normal">Welcome, <?php if(is_null($user['pseudonym']) || $user['is_pseudonym']==0){echo $user['username'];}else{echo $user['pseudonym'];} ?>,</h3>
+                <h2 class="display-5 fw-normal">Welcome, <?php if(empty($user['pseudonym']) || $user['is_pseudonym']==0){echo $user['username'];}else{echo $user['pseudonym'];} ?>,</h3>
                 <p class="lead fw-normal">Today is the <?php echo date("l jS, Y") ?></p>
                 <small>UnderCMS <?php echo $UC_VERSION ?></small>
             </div>
